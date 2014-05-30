2a80a9f1ef15953e4bbb5fd816e399aa15397559
Gavin Xie <gavin_xie@yahoo.com>
2014-05-29 22:24:03 -0400

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card{
+
+    public int index, value;
+
+    public Card(int x){
+	value=x;
+    }
+
+    public String toString(){
+	return ""+value;
+    }
+    public int getIndex(){
+	return index;
+    }
+    public int getValue(){
+	return value;
+    }
+}
