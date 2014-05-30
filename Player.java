7e619f663a19d7721b2698159e355c70b40f8d94
jonapi <jonapi10@gmail.com>
2014-05-30 14:41:33 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,11 +1,13 @@
 import java.io.*;
 import java.util.Queue;
+import java.util.ArrayList;
 import java.util.*;
 
 public class Player{
 
     public String name;
     public Queue<Card> hand=new Queue<Card>();
+    public ArrayList<Card> hand2 = new ArrayList<Card>();
 
     public Player(String name){
 	name=this.name;
