737c3dcb2f10f02a475aaabc98a4bb78901134df
Gavin Xie <gavin_xie@yahoo.com>
2014-06-03 20:24:10 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,14 +1,17 @@
 import java.io.*;
-import java.util.Queue;
-import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.*;
 
 public class Player{
 
     public String name;
-    public Queue<Card> hand=new Queue<Card>();
-    public ArrayList<Card> hand2 = new ArrayList<Card>();
+    public Queue<Card> hand=new ConcurrentLinkedQueue<Card>();
+    // Computer
+    public Player(){
+	name="Comp";
+    }
 
+    //Different contructor for real player
     public Player(String name){
 	name=this.name;
     }
@@ -17,9 +20,29 @@ public class Player{
 	return name;
     }
 
-    public void add(Card card){
+    //prints out the hand
+    public String printHand(){
+	Queue<Card> temp;
+	temp=hand;
+	int size=hand.size();
+	for(int x=0;x<size;x++){
+	    temp.add(hand.peek());
+	    hand.remove(hand.peek());
+	}
+	String hand="";
+	while(temp.peek()!=null){
+	    hand=hand+temp.poll()+" ";
+	}
+	return hand;
+    }
+    //adds the card to your hand
+    public void addCard(Card card){
 	hand.add(card);
     }
 
+    //takes the card out of tyour hand and returns it
+    public Card getCard(){
+	return hand.poll();
+    }
 
 }
