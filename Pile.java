1e5bc3d56b253c166b85808d26f4b1e66bbd5d5d
Gavin Xie <gavin_xie@yahoo.com>
2014-06-03 20:27:56 -0400

    Update Pile.java

--- Pile.java
+++ Pile.java
@@ -1,39 +1,53 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.*;
 
 public class Pile{
 
+    //variables
     public ArrayList<Card> pile=new ArrayList<Card>();
+    public int cardCount;
 
+    //pile is intially created having no cards
     public Pile(){
-	
+	cardCount=0;
     }
 
-    public void addCard(ArryaList<Player> players){
-	Card card=hand.poll();
+    //First card is taken from player's hand and added to the pile
+    public boolean addCard(Player player){
+	Player temp=new Player();
+	Card card=player.getCard();
+
+	//if the player cannot put any more cards in the pile, he has lost 
 	if(card.equals(null)){
-	    System.out.println(name+" has lost");
-	}else{
+	    System.out.println(player.getName()+" has lost");
+	} 
+	//if he can add a card, he does and the card count increments
+	else{
 	    pile.add(card);
+	    cardCount++;
+	
 	}
-    }
-	public Card getFirst(){
-		return pile.get(0);
+	//if the value of the card is the same as the current count 
+	if(cardCount%13==card.getValue()||card.getValue()==11){
+	    return true;
+	}else
+	    {return false;
 	}
-    public void add(Card card){
-	pile.add(Card);
     }
 
+    //the player wins the pile
     public void shuffleInto(Player hand){
+	System.out.println(hand.getName()+" has won all "+ pile.size()+" cards.");
 	while(pile.size()>0){
 	    Random rand=new Random();
-	    int x=rand.nextInt(deck.size());
+	    int a=pile.size();
+	    int x=rand.nextInt(a);
 	    hand.add(pile.get(x));
 	    pile.remove(x);
 	}
     }
-
 }
 
 		  
