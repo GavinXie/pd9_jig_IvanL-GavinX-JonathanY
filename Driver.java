03372473c2c398e96406f7c352a6e2e67f114267
Gavin Xie <gavin_xie@yahoo.com>
2014-06-03 20:23:33 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -4,7 +4,11 @@ public class Driver{
 	Deck deck= new Deck();
 	System.out.println(deck);
 	deck.shuffle();
-	System.out.println(deck.getSize());
 	System.out.println(deck);
+	Player player1=new Player();
+	Player player2=new Player();
+	deck.passCards(player1, player2);
+	System.out.println(player1.printHand());
+	System.out.println(player2.printHand());
     }
 }
