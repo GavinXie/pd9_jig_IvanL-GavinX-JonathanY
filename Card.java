c52c4e2c5b09b9283a3bcfe2f969a53cdf457ff0
Gavin Xie <gavin_xie@yahoo.com>
2014-06-15 17:12:42 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,34 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class Card{
 
-    private int index, value, suit;
-    
+    public int index, value, suit, x,y;
+    protected Image image;
+    String cardName, suitName;
 
-    public Card(int x, int suit){
-	value=x;
-	this.suit = suit;
+    public Card(int v, int s, int x, int y){
+	value=v;
+	suit=s;
+	this.x=x;
+	this.y=y;
+	if(suit==1){
+	    suitName="Diamonds";
+	}else if(suit==2){
+	    suitName="Clubs";
+	}else if(suit==3){
+	    suitName="Hearts";
+	}else{
+	    suitName="Spades";
+	}
+        cardName=value+" of "+suitName;
+        ImageIcon icon = new ImageIcon(type);
+        image = icon.getImage();
     }
 
     public String toString(){
-    	if (suit == 1){
-    		return ""+value + " of Diamonds";
-    	}
-    	else if (suit == 2){
-		return ""+value + " of Clubs";
-    	}
-    	else if (suit == 3){
-    		return ""+value + " of Hearts";
-    	}
-    	else {
-    		return ""+value + "  of Spades";
-    	}
+	return cardName;
     }
     public int getIndex(){
 	return index;
     }
     public int getValue(){
 	return value;
-    }
-    public int getSuit(){
-    	return suit;
     }
 }
