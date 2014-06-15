916c480c6891e19970b3893109a98b7e59558729
Gavin Xie <gavin_xie@yahoo.com>
2014-06-15 17:19:23 -0400

    Update Pile.java

--- Pile.java
+++ Pile.java
@@ -13,45 +13,59 @@ public class Pile{
     public Pile(){
 	cardCount=0;
     }
-    //Less restrictive add catered to allow a card to be added in the beginning of Crazy Eights.
-    public void addCard(Card a){
-    	pile.add(a);
-    	cardCount++;
-    	System.out.println(a);
+
+    public String toString(){
+	String pilePrint ="Pile: ";
+	for(int x=0;x<pile.size();x++){
+	    pilePrint=pilePrint+pile.get(x)+" ";
+	}
+	return pilePrint;
     }
+    
+    public Card getTop(){
+	return pile.get(pile.size()-1);
+    }
+
     //First card is taken from player's hand and added to the pile
-    public boolean addCard(Player player){
+    public void addCard(Player player){
 	Player temp=new Player();
 	Card card=player.getCard();
-
+	/*
 	//if the player cannot put any more cards in the pile, he has lost 
 	if(card.equals(null)){
 	    System.out.println(player.getName()+" has lost");
 	} 
 	//if he can add a card, he does and the card count increments
-	else{
+	*/
+	
 	    pile.add(card);
 	    cardCount++;
-	
-	}
+    }
+
+    public boolean shouldSlap(){
+	Card card=pile.get(pile.size()-1);
 	//if the value of the card is the same as the current count 
 	if(cardCount%13==card.getValue()||card.getValue()==11){
 	    return true;
-	}else
-	    {return false;
+	}else{
+	    return false;
 	}
     }
 
     //the player wins the pile
     public void shuffleInto(Player hand){
 	System.out.println(hand.getName()+" has won all "+ pile.size()+" cards.");
-	while(pile.size()>0){
+	int a=pile.size();
+	for(int i=0;i<a;i++){
+	    System.out.println("WorkPlaceX"+pile.size());
 	    Random rand=new Random();
-	    int a=pile.size();
-	    int x=rand.nextInt(a);
-	    hand.add(pile.get(x));
+	    int r=pile.size();
+	    int x=rand.nextInt(r);
+	    hand.addCard(pile.get(x));
 	    pile.remove(x);
+	    System.out.println(hand);
 	}
+	System.out.println("Mission Accomplished");
     }
 }
 
