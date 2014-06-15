c1918369b2219b1efd98a29bb53a17b2a60318f1
Gavin Xie <gavin_xie@yahoo.com>
2014-06-15 17:23:21 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,41 +1,19 @@
-public class Driver{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-    public static void slapJack(Player player1, Player player2){
-	Deck deck= new Deck();
-	System.out.println(deck);
-	deck.shuffle();
-	System.out.println(deck);
-	deck.passCards(player1, player2);
-	System.out.println("Player 1: "+ player1.printHand());
-	System.out.println("Player 2: "+ player2.printHand());
-	System.out.println("Apparently player.printHand()the player toString removes the data...");
-	System.out.println("Player 1: "+ player1.printHand());
-	System.out.println("Player 2: "+ player2.printHand());
-	Pile pile = new Pile();
-	boolean goodSlap=true;
-	while(player1.hasCards()&&player2.hasCards()){
-	    pile.addCard(player1);
-	    goodSlap= player1.toSlap(pile);
-	    if(goodSlap){
-		pile.shuffleInto(player1);
-	    }else{
-		pile.shuffleInto(player2);
-	    }
-	    pile.addCard(player2);
-	    goodSlap= player2.toSlap(pile);
-	    if(goodSlap){
-		pile.shuffleInto(player2);
-	    }else{
-		pile.shuffleInto(player1);
-	    }
-	    System.out.println("Player 1: " + player1.printHand());
-	    System.out.println("Player 2: " + player2.printHand());
-	}
+public class Driver extends JFrame{
+    public Driver(){
+	add(new GUI());
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setSize(900,600);
+	setLocationRelativeTo(null);
+	setTitle("SlapJack");
+	setResizable(false);
+	setVisible(true);
     }
 
     public static void main(String[] args){
-	Player player1 = new Player();
-	Player player2 = new Player();
-	slapJack(player1, player2);
+	new Driver();
     }
 }
