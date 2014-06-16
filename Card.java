ad17be317c2801ba8ff4e33f09c218e780e87e50
jonapi <jonapi10@gmail.com>
2014-06-16 00:49:22 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -6,13 +6,24 @@ public class Card{
 
     public int index, value, suit, x,y;
     protected Image image;
-    String cardName, suitName;
+    String cardName, suitName,valueName;
 
     public Card(int v, int s, int x, int y){
 	value=v;
 	suit=s;
 	this.x=x;
 	this.y=y;
+	if (value == 1){
+	    valueName = "Ace";
+	}else if(value == 11){
+	    valueName = "Jack";
+	}else if (value == 12){
+	    valueName = "Queen";
+	}else if (value == 13){
+	    valueName = "King";
+	}else{
+	    valueName = value;
+	}
 	if(suit==1){
 	    suitName="Diamonds";
 	}else if(suit==2){
@@ -22,7 +33,7 @@ public class Card{
 	}else{
 	    suitName="Spades";
 	}
-        cardName=value+" of "+suitName;
+        cardName=valueName+" of "+suitName;
         ImageIcon icon = new ImageIcon(cardName);
         image = icon.getImage();
     }
