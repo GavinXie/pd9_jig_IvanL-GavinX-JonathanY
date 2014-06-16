d5944629fd86ac257c40a291ed0cf4e00574de59
Gavin Xie <gavin_xie@yahoo.com>
2014-06-16 04:38:44 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 import java.io.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.*;
@@ -6,36 +9,79 @@ import java.util.Scanner;
 public class Player{
 
     public String name,slapKey,noSlapKey;
+    public int x,y, rotation;
+    public boolean slapped;
     public Queue<Card> hand=new ConcurrentLinkedQueue<Card>();
     public ArrayList<Card> UnoHand = new ArrayList<Card>();
-
     // Computer
-    public Player(){
-	name="Comp";
+    public Player(int c){
+	name="Comp"+c;
+	if(c==1){
+	    x=414;
+	    y=500;
+	    rotation=0;
+	}else if(c==2){
+	    x=800;
+	    y=264;
+	    rotation=90;
+	}else if(c==3){
+	    x=414;
+	    y=4;
+	    rotation=180;
+	}else{
+	    x=4;
+	    y=264;
+	    rotation=270;
+	}
     }
 
     //Different contructor for real player
-    public Player(String name){
+    public Player(String name, int c){
 	this.name=name;
+	if(c==1){
+	    x=414;
+	    y=500;
+	    rotation=0;
+	}else if(c==2){
+	    x=800;
+	    y=264;
+	    rotation=90;
+	}else if(c==3){
+	    x=414;
+	    y=4;
+	    rotation=180;
+	}else{
+	    x=4;
+	    y=264;
+	    rotation=270;
+	}
+    }
+
+    public int getX(){
+	return x;
+    }
+    public int getY(){
+	return y;
     }
 
     public String getName(){
 	return name;
     }
 
+    public boolean hasSlapped(){
+	return slapped;
+    }
+
     public String toString(){
 	return name+": "+printHand();
     }
-    public String toString2(){
-	String Open = "[ ";
-	for (int i = 0; i < UnoHand.size(); i++){
-	    Open += UnoHand.get(i) +" (" + i + ")" + ", ";
-	}
-	Open+= " ]";
-	return Open;
-    }
+
     public boolean hasCards(){
 	return !hand.isEmpty();
+    }
+
+    public Queue<Card> getHand(){
+	return hand;
     }
 
     //prints out the hand
@@ -69,6 +115,24 @@ public class Player{
 	    return true;
 	}else{
 	    return false;
+	}
+    }
+
+    public synchronized void keyPressed(KeyEvent e){	
+	int key = e.getKeyCode();
+	if(key == KeyEvent.VK_SPACE){
+	    slapped=true;
+	}else{
+	    slapped=false;
+	}
+    }
+
+    public synchronized void keyReleased(KeyEvent e){	
+	int key = e.getKeyCode();
+	if(key == KeyEvent.VK_SPACE){
+	    slapped=false;
+	}else{
+	    slapped=true;
 	}
     }
 
